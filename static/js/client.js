6adf0f59f5b566df5a7869671dd237deb58c3ab1
jimsiff <jim@sifferle.net>
2014-11-19 23:28:51 -0800

    Streamline code

--- static/js/client.js
+++ static/js/client.js
@@ -291,17 +291,13 @@
                 var prevfocusPoint = nowData[nowData.length - 2];
 
                 //in this case the SGV is scaled
-                if (focusPoint.y < 40) {
+                $('.container .currentDelta').text('');
+                if (focusPoint.y < 40) 
                     $('.container .currentBG').text('LOW');
-                    $('.container .currentDelta').text('');
-                }
-                else if (focusPoint.y > 400) {
+                else if (focusPoint.y > 400) 
                     $('.container .currentBG').text('HIGH');
-                    $('.container .currentDelta').text('');
-                }
-                else {
+                else 
                     $('.container .currentBG').text(focusPoint.sgv);
-                    $('.container .currentDelta').text('');
                     if (prevfocusPoint.y >= 40) {
                         var retroDelta = scaleBg(focusPoint.y) - scaleBg(prevfocusPoint.y);
                         if (browserSettings.units == "mmol") {
@@ -323,7 +319,6 @@
                             .text(retroDeltaString)
                             .css('text-decoration','line-through');
                     }
-                }
                 $('.container .currentBG').css('text-decoration','line-through');
                 $('.container .currentDirection').html(focusPoint.direction)
             } else {
@@ -392,17 +387,13 @@
                 $('#lastEntry').text(timeAgo(secsSinceLast)).toggleClass('current', secsSinceLast < 10 * 60);
 
                 //in this case the SGV is unscaled
-                if (latestSGV.y < 40) {
+                $('.container .currentDelta').text('');
+                if (latestSGV.y < 40)
                     $('.container .currentBG').text('LOW');
-                    $('.container .currentDelta').text('');
-                }   
-                else if (latestSGV.y > 400) {
+                else if (latestSGV.y > 400)
                     $('.container .currentBG').text('HIGH');
-                    $('.container .currentDelta').text('');
-                }
-                else {
+                else
                     $('.container .currentBG').text(scaleBg(latestSGV.y));
-                    $('.container .currentDelta').text('');
                     if (prevSGV.y >= 40) {
     		            var bgDelta = scaleBg(latestSGV.y) - scaleBg(prevSGV.y);
                         if (browserSettings.units == "mmol") {
@@ -424,7 +415,6 @@
                             .text(bgDeltaString)
                             .css('text-decoration','');
                     }
-                }
                 $('.container .currentBG').css('text-decoration', '');
                 $('.container .currentDirection').html(latestSGV.direction);
 
